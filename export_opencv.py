9026e460d5d5be2109a0e1e8aa05c2ce932a149e
Abduragim <shtanchaev1994@gmail.com>
2024-07-01 12:25:06 +0300

    update exporting file

--- export_opencv.py
+++ export_opencv.py
@@ -1,11 +1,25 @@
 from ultralytics import YOLOv10
+import argparse
 
-print("LOADING MODEL")
-model = YOLOv10.from_pretrained("jameslahm/yolov10s")
+if __name__ == "__main__":
 
-print("EXPORTING MODEL TO ONNX")
-model.export(format='onnx', simplify=False, imgsz=(480, 640))
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model", type=str,
+                        default="yolov10s",
+                        help="Model name to export to onnx",
+                        choices=["yolov10n", "yolov10s", "yolov10m", "yolov10b", "yolov10l", "yolov10x"])
 
-print("FORWARD PASS")
-results = model.predict(source="./sample-image.png",  conf=0.5)
-print("END")
+    parser.add_argument("--imgsz", type=int, nargs=2,
+                        default=(480, 640),
+                        help="Image size for the model")
+    args = parser.parse_args()
+
+    print("LOADING MODEL")
+    model = YOLOv10.from_pretrained(f"jameslahm/{args.model}")
+
+    print("EXPORTING MODEL TO ONNX")
+    model.export(format='onnx', simplify=False, imgsz=args.imgsz)
+
+    print("FORWARD PASS")
+    results = model.predict(source="./sample-image.png",  conf=0.5)
+    print("END")
